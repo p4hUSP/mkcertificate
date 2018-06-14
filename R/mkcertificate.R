ee6cbb6e85b430c1ba3e4720bd99b5f3b14cf006
Rafael Coelho <rafael.coelho.x@gmail.com>
2018-06-14 12:17:37 -0400

    função funciona. Falta generalizar agora

--- R/mkcertificate.R
+++ R/mkcertificate.R
@@ -2,32 +2,37 @@
 #'
 library(magrittr)
 
-mkcertificate <- function(data_list, border = "border.jpg", ass_paths = "R/PRIMEIRAASSINATURA.png"){nam
+mkcertificate <- function(data_list, ass_paths = "DEMO/PRIMEIRAASSINATURA.png"){
   names_vector <- data[[col_name]]
+
   texto_base <- mkcertificate::template
+
   texto_uso <- texto_base %>%
-    stringr::str_replace("NOMEALUNO", NOME_DO_ALUNO) %>%
-    stringr::str_replace("HORASCOMPLETAS", HORAS_COMPLETAS) %>%
+    stringr::str_replace("NOMEALUNO", "NOMEDOALUNO") %>%
+    stringr::str_replace("HORASCOMPLETAS", "HORASCOMPLETAS") %>%
     stringr::str_replace("PRIMEIRAASSINATURA", normalizePath(ass_paths)) %>%
     stringr::str_replace("SEGUNDAASSINATURA", normalizePath(ass_paths))
 
   temp_dir <- tempdir()
 
-  border <- magick::image_read("")
+  border <- magick::image_read("https://raw.githubusercontent.com/p4hUSP/mkcertificate/master/borders/border-1.jpg")
 
-  texto_base <- stringr::str_replace(texto_base, "BORDERPATH", normalizePath(border))
+  border_path <- stringr::str_c(temp_dir, "/border.jpg")
+
+  magick::image_write(border, path = border_path)
+
+  texto_uso <- stringr::str_replace(texto_uso, "BORDERPATH", "border.jpg")
 
   temp_file <- tempfile(tmpdir = temp_dir)
 
-  writeLines(text, con = temp_file)
+  writeLines(texto_uso, con = temp_file)
 
-  {
-    current_wd <- getwd()
-    setwd(temp_dir)
-    system(sprintf("pdflatex %s", temp_file))
-    setwd(current_wd)
-    system(sprintf("cp %s.pdf teste.pdf", temp_file))
-  }
+  current_wd <- getwd()
+  setwd(temp_dir)
+  system(sprintf("pdflatex %s", temp_file))
+  setwd(current_wd)
+  system(sprintf("cp %s.pdf teste.pdf", temp_file))
+
 }
 # template <- readLines(con = file("R/certificate_template.tex"))
 #
