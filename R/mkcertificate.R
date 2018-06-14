b90027a204a941adf86c6f7b0e4c3c051f874730
Rafael Coelho <rafael.coelho.x@gmail.com>
2018-06-14 14:19:58 -0400

    fix

--- R/mkcertificate.R
+++ R/mkcertificate.R
@@ -1,6 +1,9 @@
+#' Create Certificates of Attendance
 #'
+#' \code(mkcertificate) creates certificates of attendance based on
+#' data.
 #'
-library(magrittr)
+#' @export
 
 mkcertificate <- function(data, col_names, col_hours, col_email, col_id, ass_paths1, ass_paths2, ex_dir = "."){
 
@@ -9,20 +12,11 @@ mkcertificate <- function(data, col_names, col_hours, col_email, col_id, ass_pat
   purrr::pmap(lista, build_certificate, ass_paths1, ass_paths2, ex_dir)
 }
 
-org_data <- function(data, col_names, col_hours, col_email){
-
-  names <- data[[col_names]]
-
-  hours <- as.character(data[[col_hours]])
-
-  email <- data[[col_email]]
-
-  lista <- list(names = names,
-                hours = hours,
-                email = email)
-
-  return(lista)
-}
+#' Build the Certificate
+#'
+#' \code(build_certificate) is the function responsible to generate the PDF.
+#'
+#' @export
 
 build_certificate <- function(names, hours, email, ass_paths1, ass_paths2, ex_dir){
   texto_base <- mkcertificate::template
@@ -35,7 +29,7 @@ build_certificate <- function(names, hours, email, ass_paths1, ass_paths2, ex_di
 
   temp_dir <- tempdir()
 
-  border <- magick::image_read("https://raw.githubusercontent.com/p4hUSP/mkcertificate/master/borders/border-1.jpg")
+  border <- magick::image_read("https://raw.githubusercontent.com/p4hUSP/mkcertificate/master/inst/extdata/border-1.jpg")
 
   border_path <- stringr::str_c(temp_dir, "/border.jpg")
 
@@ -54,3 +48,19 @@ build_certificate <- function(names, hours, email, ass_paths1, ass_paths2, ex_di
   system(sprintf("cp %s.pdf %s/%s.pdf", temp_file, ex_dir, digest::digest(email)))
 }
 
+#'
+
+org_data <- function(data, col_names, col_hours, col_email){
+
+  names <- data[[col_names]]
+
+  hours <- as.character(data[[col_hours]])
+
+  email <- data[[col_email]]
+
+  lista <- list(names = names,
+                hours = hours,
+                email = email)
+
+  return(lista)
+}
